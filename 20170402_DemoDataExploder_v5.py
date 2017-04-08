404fc5c2be64e91e9bc78f8762768a06267c9b6a
jrunrun <jcraycraft@jcraycraft-mac.tsi.lan>
2017-04-08 11:48:11 -0500

    clean up

--- 20170402_DemoDataExploder_v5.py
+++ 20170402_DemoDataExploder_v5.py
@@ -6,8 +6,6 @@ import math
 import time
 import locale
 
-
-
 print "Demo data exploder starting..."
 
 # Start stopwatch
@@ -32,11 +30,8 @@ today = pd.to_datetime('today')
 start_dates = today - dt.timedelta(days = 1460)
 #create list that includes every day over last 4 years (1460 days)
 df_dates3 = pd.date_range(start=start_dates, end=today, freq = 'D').tolist()
-out_df = pd.DataFrame()
-final_df = pd.DataFrame()
-outData_df = pd.DataFrame()
 
-#this works; just can't access object in current format (timestamp)
+#sort all the dates by weekday/year for list of 4 years (1460 days) worth of days
 weekdays = [0,1,2,3,4,5,6]
 years = [2013,2014,2015,2016,2017]
 datesByWeekday = []
@@ -49,41 +44,22 @@ for idx, year in enumerate(years):
         datesByYearAndWeekday.append(result1)
     final.append(datesByYearAndWeekday) 
 
+#manually set indices for each weekday/year combo
 indy_dict = {'Mon':[0,7,14,21,28],'Tues':[1,8,15,22,29],'Wed':[2,9,16,23,30],'Thur':[3,10,17,24,31],'Fri':[4,11,18,25,32],'Sat':[5,12,19,26,33],'Sun':[6,13,20,27,34]}
 avgVar_dict = {'Mon':[260,45],'Tues':[277,41],'Wed':[250,20],'Thur':[230,30],'Fri':[223,35],'Sat':[150,20],'Sun':[145,20]}
 
-#create master list of all possible indices from source
+#create master list of all possible indices from source (option: row level or ID level of detail)
+#row level
 #df1_idx = df1.index.values.tolist()
+#ID level; specify ID; e.g. 'Uniqueid'
 df1_idx = df1['Uniqueid'].unique().tolist()
 
-#need YoY growth indexed
+#YoY growth; for now this is the same across all years; enhance later to vary by year
 growth = [[1.0,.03],[1.15,.05],[1.175,.07],[1.20,.09],[1.225,.10]]
 #growth[0][0]=1
 #growth[0][1]=.03
 
-
-YoY_dict = {'1st':[1.0,.03],'2nd':[1.15,.05],'3rd':[1.175,.07],'4th':[1.20,.09],'5th':[1.225,.10]}
-
-#cycle thru all the weekdays
-#start with first weekday = Monday, iterate across all years 
-#then for each day blow out the rows using multipliers based off the dictionaries
-  
-#USE LATER FOR ITERATING ACROSS DICTIONARIES    
-#create n rows for specified date
-#for (k,v), (k2,v2) in zip(avgVar_dict.items(), indy_dict.items()):
-#prove first attempt with today's date; then improve with other dates
-
-#declare global objects
-
-out_df = pd.DataFrame() 
-x1_df = pd.DataFrame()
-x2_df = pd.DataFrame()
-
-#static n, but adjust in final version
-#n = 25
-
 #function that returns dataframe of random indices (given list "df1_idx") and associated date (given date)
-
 def df_builder(indices,rows,date):
     rows = int(rows)
     df_tmp = pd.DataFrame()
@@ -92,49 +68,27 @@ def df_builder(indices,rows,date):
     df_tmp['date'] = date
     return(df_tmp)
 
+#containers
 out1=[]
 output1=[]
-
 out2=[]
 output2=[]
 
+#weekday index cycle
 for (k,v), (k2,v2) in zip(avgVar_dict.items(), indy_dict.items()):
-    #use k2,v2 index to select slice of dates
-    #pass to tmpDates
     tmp_iterator = v2
-    print(v2)
-
-        
-    #print(k,k2)
+    
+    #year index cycle
     for idx, tmp_item in enumerate(tmp_iterator): 
         
-        #tmpDates = final[0][v2[tmp_item]]
         tmpDates = final[0][v2[idx]]
-        #print(tmpDates)
-        #v[idx] is number of rows
-        #print(v[0],v[1])
-        #static n, but adjust in final version
-    
-       
-        #factor = growth[idx][0]
         factor = math.pow(growth[1][0],idx)
-        print(factor)
-        #print(type(factor))
-
-        #n = v[0] + random.randint(-v[1],v[1])
         n = v[0]
         n = float(n)
 
         n = n * factor
-        print(type(n))
-        print(n)
-        #dynamically set n
-        #n = v[0]
-        #3
         tmp_df3 = pd.DataFrame()
-
         out1 = (df_builder(df1_idx,n,date) for date in tmpDates)
-
 
         for out_val in out1:
             output1 = pd.concat(out1)
@@ -145,11 +99,23 @@ for (k,v), (k2,v2) in zip(avgVar_dict.items(), indy_dict.items()):
         output2 = pd.concat(out2)
 
     output2['Uniqueid'] = output2['indices']
-    output2.drop('indices', axis=1)      
+    output2 = output2.drop('indices', axis=1)      
     output2.to_csv('2017_04_06_output2_v5.csv', index = False)
 
 df1.to_csv('2017_04_06_df1_v5.csv', index = False)
 DemoData = pd.merge(df1, output2, on='Uniqueid', how='inner')
+
+#create quantities for both dist and tail for LOS
+#use this for discharge dates
+df_rows = len(DemoData)
+print("rows: ",df_rows)
+df_dist = int(len(DemoData) * 0.8)
+print("Normdist: ",df_dist)
+df_tail = df_rows - df_dist
+print("tail: ",df_tail)
+
+
+
 DemoData.to_csv(outputCSV, index = False)
 
 print("------------------------------------------------")
